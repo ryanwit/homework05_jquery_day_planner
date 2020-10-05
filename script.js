14f63b0ad5e0403e7528e4111097afc535131973
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-04 19:43:51 -0600

    Updated JS File

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ $(document).ready(function()
 
         })
 
-
+        function updateHour() {
+            var currentTime = moment().hours()
+            $(".time-block").each(function() {
+                var hour = parseInt($(this).attr("id").split("-")[1])
+                    if(hour < currentTime) {
+                        $(this).addClass("past")
+                    } else if (hour === currentTime) {
+                        $(this).removeClass("past")
+                        $(this).addClass("present")
+                    } else {
+                        $(this).removeClass("past")                    }
+            }
+        }
 
 
 )
